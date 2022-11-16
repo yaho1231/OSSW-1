ab5b7dd1f735e7a0f4ef1bc0e6e82b0e74a9b99c
jungchoib <jungchoi1@naver.com>
2022-11-16 16:39:40 +0900

    dcbot

---
+++ 221116/index.js
@@ -0,0 +1,32 @@
+/*
+const Discord = require('discord.js');	// discord.js 라이브러리 호출
+const Client = new Discord.Client();	// Client 객체 생성
+*/
+const dotenv = require("dotenv");
+dotenv.config()
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = (module.exports = new Client({intents:[131071]}));
+
+const date = new Date();
+
+// 봇과 서버를 연결해주는 부분
+client.login('MTA0MjMwODY0MzcyODY2NjY4NA.GcG6mz.Aa2W8tKDDtj1mtB7nwEUdDSheqC4JLQ8GWAtgw');
+
+// discord 봇이 실행될 때 딱 한 번 실행할 코드를 적는 부분
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+// 디스코드 서버에 작성되는 모든 메시지를 수신하는 리스너
+client.on("messageCreate", (message) => {
+	if ((message.content == "안녕") || (message.content == "hi")){
+		message.reply({content:`안녕하세요`});
+	}
+	if (message.content == "지금 몇시야" || message.content == "몇시" || message.content == "time"){
+		message.reply({content:(date.toLocaleString('ko-kr'))});
+	}
+});
+
+
