773077875185deee1f5dc7b911dbdd138500af06
yaho1231 <hiimjungle@naver.com>
2022-12-05 13:29:59 +0900

    24always

--- jstest.js
+++ jstest.js
@@ -5,6 +5,7 @@ const client = (module.exports = new Client({intents:[131071]}));
 const axios = require('axios');
 const cheerio = require('cheerio');
 const date = new Date();
+const keepAlive = require('./server.js')
 const getHtml = async () => {
     try {
       return await axios.get('https://www.hanyang.ac.kr/web/www/re12');
@@ -12,11 +13,11 @@ const getHtml = async () => {
       console.error(error);
     }
   };
-// 봇과 서버를 연결해주는 부분
-client.login('MTAzOTc5OTUyNzIwMjM2MTM4NA.Gl4e64.gyPrZ2-iWsWlBycwvIq8MiwdEJ7QbdroF7G6oo');
+
 
 // discord 봇이 실행될 때 딱 한 번 실행할 코드를 적는 부분
 client.once('ready', () => {
+  client.user.setActivity('opensourcebot',{type:'WATCHING'})
 	console.log('Ready!');
 });
 
@@ -49,34 +50,16 @@ client.on("messageCreate", (message) => {
               .replace(/[\n\t\&amp;\공통찬]/g, '')
               ,
             };
+            if(data.mainContents == ''){
+              data.mainContents = "오늘은 학식이 없습니다.";
+              data.secondContents = "오늘은 학식이 없습니다.";
+            }
             return data;
         }) //학식, 공통찬 출력
-        .then((res) => message.reply({content:("(학식)\n" +res.mainContents + "\n" +res.secondContents + "\n[공통] " + res.thirdContents)}));
+        .then((res) => message.reply({content:("(학식) 운영시간 : 11:30 ~ 13:30\n" +res.mainContents + "\n" +res.secondContents + "\n[공통] " + res.thirdContents)}));
 		
 	}
-    //코로나 api 시도했으나 실패
-    // if ((message.content == "코")){
-    //     fetch('https://api.corona-19.kr/korea/beta/?serviceKey={shlPLJDHSB2XoigwjUK1x65YbyZ3W47v9}')
-    //     .then((response) => {
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //         console.log( "=== [ " + data.updateTime + "상황 ] ===\n\n" + 
-    //         "국내 확진자: " + data.TotalCase + "\n" + 
-    //         "국내 완치자: " + data.TotalRecovered + "\n" + 
-    //         "국내 사망자: " + data.TotalDeath + "\n" + 
-    //         "국내 치료중: " + data.NowCase + "\n\n" +
-    //         "더 자세한 정보는 https://corona-19.kr/ 에서 확인하세요.");
-    //         // message.reply({content:결과});
-    //     })
-    //     .catch(() => {
-    //         message.reply({content:`error`});
-    //     })
-	// }
 });
-/*
-thumbnail-list-wrap container
-    thumbnails
-        span3
-        #yui_patched_v3_11_0_1_1669794498888_201 > h3
-*/
+keepAlive();
+// 봇과 서버를 연결해주는 부분
+client.login(process.env['TOKEN']);
